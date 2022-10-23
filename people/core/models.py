2b148a39dae83ad393ba1927b36cab65bdc9622e
Christoph Buelter <buelter.christoph@gmail.com>
2022-10-23 10:53:31 +0200

    Add User fields needed to represent it with Fabric.js

--- people/core/models.py
+++ people/core/models.py
@@ -4,4 +4,10 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    avatar = models.ImageField(upload_to="avatars", default=None, null=True, blank=True)
+
+    # Additional fields to support representation as a Fabric.js object.
+    x = models.IntegerField(default=0)
+    y = models.IntegerField(default=0)
+    angle = models.IntegerField(default=0)
+    scale = models.FloatField(default=1.0)
--- people/settings.py
+++ people/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "core.User"
+
+MEDIA_ROOT = BASE_DIR / "media"
