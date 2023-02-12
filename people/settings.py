de90b78761d6fff46e31487e23c793e15d333e42
Christoph Buelter <buelter.christoph@gmail.com>
2023-02-12 12:21:43 +0100

    Simplify dev-server setup via vite's server.proxy

--- people/settings.py
+++ people/settings.py
@@ -29,18 +29,18 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
 ]
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:8000",
-    "http://localhost:9000",
-]
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3000",
-    "http://localhost:3000",
-    "http://localhost:9000",
-]
-CORS_ALLOW_CREDENTIALS = True
-SESSION_COOKIE_SAMESITE = "None"
+# CSRF_TRUSTED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://localhost:8000",
+#     "http://localhost:9000",
+# ]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://127.0.0.1:3000",
+#     "http://localhost:3000",
+#     "http://localhost:9000",
+# ]
+# CORS_ALLOW_CREDENTIALS = True
+# SESSION_COOKIE_SAMESITE = "None"
 
 # Application definition
 
